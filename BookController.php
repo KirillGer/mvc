6224b874133669f9156e6983c7e69a82dd455ef8
KirillGer <116500910+KirillGer@users.noreply.github.com>
2022-12-24 14:55:49 +0500

    Update BookController.php

--- BookController.php
+++ BookController.php
@@ -11,16 +11,27 @@ class BookController {
 
 		return $books->getData();
 	}
-
-	public function __construct(protected $newData)
+	public function __construct(array $newData, string $name_book, string $author, string $category)
 	{
 		$newData = new BookModel;
 		$newData = [];
-		
+		$name_book = 'Fathers & Childrens';
+		$this->name = $name_book;
+		$author = 'Turgenev Ivan Sergeyevich';
+		$this->author = $author;
+		$category = 'romance';
+		$this->category = $category;
+		array_push($newData, $name_book);
+		array_push($newData, $author);
+		array_push($newData, $category);
+		$this->newData = $newData;
+		array_push($data, $newData);
 	}
 
-	public function __construct(protected $deleteData)
+	public function __construct()
 	{
-		
+		unset($data['name_book']);
+		unset($data['author']);
+		unset($data['category']);
 	}
 }
